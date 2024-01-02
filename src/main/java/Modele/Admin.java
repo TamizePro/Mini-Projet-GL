8285c91aa6cb7df28b46ab40b07c4324088aaf96
AJude <tamizepro@gmail.com>
2024-01-02 19:04:39 +0100

    Implementation du design pattern singleton avec la classe Admin et ajout des dependences aux fichier pom.xml

--- src/main/java/Modele/Admin.java
+++ src/main/java/Modele/Admin.java
@@ -3,6 +3,8 @@ package Modele;
 public class Admin {
     private static  Admin instance;
     private Compte compte;
+    private String nom;
+    private String prenom;
 
     private Admin(Compte compte) {
         this.compte = compte;
@@ -21,5 +23,21 @@ public class Admin {
 
     public void setCompte(Compte compte) {
         this.compte = compte;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 }
