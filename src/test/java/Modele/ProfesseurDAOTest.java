8c99fa9962269728abffbd5e50bf6f2d8f77810e
GNEME Gilles <gillesgnemure@gmail.com>
2024-01-05 00:38:56 +0100

    Tests done

--- src/test/java/Modele/ProfesseurDAOTest.java
+++ src/test/java/Modele/ProfesseurDAOTest.java
@@ -3,11 +3,15 @@ package Modele;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 
 import java.sql.*;
 import java.util.List;
 import java.util.ArrayList;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProfesseurDAOTest {
 	private ProfesseurDAO professeurDAO;
 
@@ -17,41 +21,44 @@ public class ProfesseurDAOTest {
 	}
 
 	@Test
+	@Order(1)
 	public void shouldAddProfesseur() {
 		// TODO: initialize args
 		Professeur professeur;
 		professeur = new ProfesseurBuilder("Nom",
-			"Prenom", "Maths", 12L).compte(new Compte("bourama", "hello")).build();
+			"Prenom", "Maths", 12L).compte(new Compte("gg", "ggpwd")).build();
 		boolean actualValue = professeurDAO.AddProfesseur(professeur);
 		// TODO: assert scenario
 		assertTrue(actualValue, "Adding compte should return true on success");
 	}
 
 	@Test
+	@Order(2)
 	public void shouldDeleteProfesseur() {
 		// TODO: initialize args
 		Professeur professeur;
-		professeur = new ProfesseurBuilder("Nom",
-			"Prenom", "Maths", 12L).compte(new Compte("bourama", "hello")).build();
+		professeur = new ProfesseurBuilder("Nom", "Prenom", "Maths", 11L).compte(new Compte("alain", "hello")).build();
 		boolean actualValue = professeurDAO.DeleteProfesseur(professeur);
 		// TODO: assert scenario
 		assertTrue(actualValue, "Deleting compte should return true on success");
 	}
 
 	@Test
+	@Order(3)
 	public void shouldUpdateProfesseurByCode() {
 		// TODO: initialize args
 		Professeur professeur;
 		Long code;
 		code = 13L;
 		professeur = new ProfesseurBuilder("Hello",
-			"World", "Maths", code).compte(new Compte("gg", "ggpwd")).build();
+			"World", "Maths", code).compte(new Compte("bourama", "hello")).build();
 		boolean actualValue = professeurDAO.UpdateProfesseurByCode(professeur, code);
 		// TODO: assert scenario
 		assertTrue(actualValue, "Updating compte should return true on success");
 	}
 
 	@Test
+	@Order(4)
 	public void shouldGetProfesseurByCode() {
 		// TODO: initialize args
 		Long code;
@@ -64,17 +71,17 @@ public class ProfesseurDAOTest {
 		assertEquals(realval, actualValue, "Objects should be equals");
 	}
 
-	/*@Test
+	@Test
+	@Order(5)
 	public void shouldGetAllProfesseur() {
-		professeurDAO.UpdateProfesseurByCode(new ProfesseurBuilder("Nom", "Prenom", "Maths", 13L)
-		.compte(new Compte("bourama", "hello"))
-		.build(), 13L);
 		List<Professeur> actualValue = professeurDAO.GetAllProfesseur();
 		List<Professeur> realval = new ArrayList();
-		realval.add(new ProfesseurBuilder("Nom", "Prenom", "Maths", 13L).build());
-		realval.add(new ProfesseurBuilder("Nomd", "Prenoms", "Maths", 14L).build());
+		realval.add(new ProfesseurBuilder("Nom", "Prenom", "Maths", 12L).compte(new Compte("gg", "ggpwd")).build());
+		realval.add(new ProfesseurBuilder("Hello", "World", "Maths", 13L).compte(new Compte("bourama", "hello")).build());
+		realval.add(new ProfesseurBuilder("Nomd", "Prenoms", "Maths", 14L).compte(new Compte("admin_admin",
+			"21232f297a57a5a743894a0e4a801fc3")).build());
 		// TODO: assert scenario
 		assertTrue(realval.size() == actualValue.size() && realval.containsAll(actualValue)
 			&& actualValue.containsAll(realval), "List should be the same");
-	}*/
+	}
 }
